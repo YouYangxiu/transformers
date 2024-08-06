c23b80a417f1a1b2698333b2f25b3279520c3322
josephyou <josephyou@tencent.com>
2024-08-06 18:35:05 +0800

    add device

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -291,7 +291,6 @@ class Qwen2Attention(nn.Module):
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
 
 
-        print(f"layer_idx...{self.layer_idx}")
         if self.layer_idx == 0:
             print(f"this is attention mask from class Qwen2Attention {attention_mask}")
 
