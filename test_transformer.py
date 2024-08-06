64161124bce85b2835f93ad154d9302beb0f69f8
josephyou <josephyou@tencent.com>
2024-08-06 16:21:43 +0800

    test pip install -e .

--- test_transformer.py
+++ test_transformer.py
@@ -4,3 +4,27 @@
 @Author: josephyou
 @Email: josephyou@tencent.com
 """
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+device = "cuda" # the device to load the model onto
+transformers.logging.set_verbosity_error()
+
+
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-1.8B-Chat", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-1.8B-Chat")
+
+prompt = "Give me a short introduction to large language model."
+
+messages = [{"role": "user", "content": prompt}]
+
+text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=100, do_sample=False, top_p=None)
+
+generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(response)
