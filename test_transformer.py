0a4b551df3f3b53b3bdc065bb6d91f83800de681
josephyou <josephyou@tencent.com>
2024-08-07 10:24:37 +0800

    1

--- test_transformer.py
+++ test_transformer.py
@@ -19,13 +19,13 @@ tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-1.8B-Chat")
 
 prompt = "Give me a short introduction to large language model."
 
-# messages = [{"role": "user", "content": prompt}]
+messages = [{"role": "user", "content": prompt}]
 #
-# text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-model_inputs = tokenizer([prompt], return_tensors="pt").to("cuda:0")
+model_inputs = tokenizer([text], return_tensors="pt").to("cuda:0")
 
-generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=1000, do_sample=False, top_p=None)
+generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=100, do_sample=False, top_p=None)
 
 generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
 
